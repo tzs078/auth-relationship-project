608e5c01522c2ac286b05b1ce14817bb42216f6c
tzs078 <tasnimtusu07+py@gmail.com>
2026-06-21 12:39:36 +0600

    Delete new_app directory

--- new_app/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from new_app.models import *
-# Register your models here.
-admin.site.register(UserModel)
--- new_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class UserModel(AbstractUser):
-    full_name = models.CharField(max_length=200, null=True)
-
-    def __str__(self):
-        return f'{self.username}'
